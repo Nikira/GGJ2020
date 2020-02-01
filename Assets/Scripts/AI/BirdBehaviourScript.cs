d4809c0236b0037d3fbd91b463000bfc4d360ff9
Miriam Popescu <qbus89@gmail.com>
2020-02-01 13:20:50 +0100

    sprite renderer on birb

--- Assets/Scripts/AI/BirdBehaviourScript.cs
+++ Assets/Scripts/AI/BirdBehaviourScript.cs
@@ -5,9 +5,19 @@ using UnityEngine;
 public class BirdBehaviourScript : MonoBehaviour
 {
     public BirdControllerScript parentController; // Initialized by controller
+    public const string LAYER_NAME = "Foreground";
+    public int sortingOrder = 0;
+    private SpriteRenderer sprite;
 
     public void OnDestroy()
     {
         parentController.birds.Remove(this.gameObject);
+    }
+
+    public void SetLayer()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+        sprite.sortingOrder = sortingOrder;
+        sprite.sortingLayerName = LAYER_NAME;
     }
 }
