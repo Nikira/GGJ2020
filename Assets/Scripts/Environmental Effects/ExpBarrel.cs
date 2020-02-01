471edb9e186b41291c49d27a8332f2f3ce775a77
DaveBot <generomachina@gmail.com>
2020-02-01 21:17:25 +0100

    Added a main menu and an explosive barrel which sends all nearby rigidbodies flying when coming in contact with the blob object

---
+++ Assets/Scripts/Environmental Effects/ExpBarrel.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Explosion))]
+public class ExpBarrel : MonoBehaviour
+{
+    Explosion exploderReference;
+    public AudioClip explosionSound; // Initialized in inspector
+    AudioSource source;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        exploderReference = GetComponent<Explosion>();
+        source = GetComponent<AudioSource>();
+    }
+
+    // Explode if the blob touches it
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.GetComponent<BlobController>() != null)
+        {
+            source.PlayOneShot(explosionSound);
+            exploderReference.Explode();
+            StartCoroutine(DestroyOnDelay());
+        }
+    }
+
+    IEnumerator DestroyOnDelay()
+    {
+        GetComponent<SpriteRenderer>().enabled = false;
+        GetComponent<Collider2D>().enabled = false;
+
+        yield return new WaitForSeconds(2f);
+        Destroy(this.gameObject);
+    }
+}
